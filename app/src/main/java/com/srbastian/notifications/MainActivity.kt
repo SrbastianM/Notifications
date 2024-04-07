2d8a48b50236e5f3db66d651cc842b0068f8ecd0
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-04-07 18:11:15 -0500

    Set the binding for the project

--- app/src/main/java/com/srbastian/notifications/MainActivity.kt
+++ app/src/main/java/com/srbastian/notifications/MainActivity.kt
@@ -2,10 +2,16 @@ package com.srbastian.notifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.srbastian.notifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(view)
+
+
     }
 }
