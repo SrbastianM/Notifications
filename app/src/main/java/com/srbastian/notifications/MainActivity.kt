3b214ea701115efe906c872b4cdf5129d324ee0c
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-04-08 13:06:48 -0500

    I work rn in the broadcast to show a notification using the phone clock

--- app/src/main/java/com/srbastian/notifications/MainActivity.kt
+++ app/src/main/java/com/srbastian/notifications/MainActivity.kt
@@ -1,76 +1,82 @@
 package com.srbastian.notifications
 
 import android.Manifest
+import android.app.AlarmManager
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.srbastian.notifications.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
-    private val CHANNEL_ID = "1"
-
-    private var increaseNumber = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         super.onCreate(savedInstanceState)
         setContentView(view)
 
+        val calendar = Calendar.getInstance()
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
+
         binding.btnSend.setOnClickListener {
-            increaseNumber++
-            binding.btnSend.text = increaseNumber.toString()
-            startNotification()
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(currentHour)
+                .setMinute(currentMinute)
+                .setTitleText("Set Notification Time")
+                .build()
+            timePicker.show(supportFragmentManager, "1")
+
+            timePicker.addOnPositiveButtonClickListener {
+                calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
+                calendar.set(Calendar.MINUTE, timePicker.minute)
+                calendar.set(Calendar.SECOND, 0)
+                calendar.set(Calendar.MILLISECOND, 0)
+
+                val intent = Intent(applicationContext, NotificationReceiver::class.java)
+                val pendingIntent = if (Build.VERSION.SDK_INT >= 23) {
+                    PendingIntent.getBroadcast(
+                        applicationContext,
+                        100,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                    )
+                } else {
+                    PendingIntent.getBroadcast(
+                        applicationContext,
+                        100,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                }
+
+                val alarmManager: AlarmManager =
+                    getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                alarmManager.setInexactRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    AlarmManager.INTERVAL_DAY,
+                    pendingIntent
+                )
+                Toast.makeText(applicationContext, "Set Alarm", Toast.LENGTH_LONG).show()
+            }
         }
-    }
 
-    private fun startNotification() {
-        val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel =
-                NotificationChannel(CHANNEL_ID, "1", NotificationManager.IMPORTANCE_DEFAULT)
-            val manager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-            manager.createNotificationChannel(channel)
-
-            builder.setSmallIcon(R.drawable.small_icon)
-                .setContentTitle("Title")
-                .setContentText("Small Test")
-
-        } else {
-            builder.setSmallIcon(R.drawable.small_icon).setContentTitle("Title")
-                .setContentText("Small Test")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        }
-
-        val notificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        notificationManagerCompat.notify(1, builder.build())
     }
 }
